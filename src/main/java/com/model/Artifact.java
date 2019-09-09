61b552b88e0f68255e6fc12480ce1388b37981c6
BaneleMK <banelen7x@gmail.com>
2019-09-09 14:26:05 +0200

    removed duplicates causing unsuccessful complilations

--- src/main/java/com/model/Artifact.java
+++
@@ -1,8 +0,0 @@
-package com.swingy.model;
-
-public abstract interface Artifact {
-    public int get_stats();
-    public String get_name();
-    public String get_type();
-    public int get_level();
-}
